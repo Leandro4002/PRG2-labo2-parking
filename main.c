4a05bbfd630a176dc69173826af03b53b38a68e8
leandro <leandro262009@gmail.com>
2022-05-28 08:15:41 +0200

    Utilisation de valeur un peu plus réeles

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main(void) {
 	// Exemples d'utilisation
 
    const Vehicule parking[] = {
-      voitureStandard("VD123456", "Fiat", 300, 150, 5),
-      voitureHautDeGamme("VD123455", "Jaguar", 1000, 600),
-      camionette("VD123444", "Citroen", 500)
+      voitureStandard("VD123456", "Fiat", 1130u, 1200u, 145u),
+      voitureHautDeGamme("ZH123455", "Jaguar", 1660u, 600u),
+      camionette("FR123444", "Citroen", 50u)
    };
 
    const size_t NB_VEHICULE = sizeof(parking) / sizeof(parking[0]);
