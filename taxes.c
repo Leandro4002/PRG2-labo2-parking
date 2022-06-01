2f3b874ed64cc2e12c211192cdb52d0b0762e6b3
leandro <leandro262009@gmail.com>
2022-06-01 18:04:06 +0200

    fonction de calcul de taxe pour chaque véhicule

--- taxes.c
+++ taxes.c
@@ -16,51 +16,95 @@ Compilateur    : Compilation fonctionnelle avec :
 */
 
 #include <assert.h> // Requis pour assert
+#include <math.h>   // Requis pour INFINITY
 #include "taxes.h"
+
+// Prédéclarations de fonctions internes
+double calculerTaxeVoitureStandard(const Vehicule* vehicule);
+double calculerTaxeVoitureHautDeGamme(const Vehicule* vehicule);
+double calculerTaxeVoiture(const Vehicule* vehicule);
+double calculerTaxeCamionette(const Vehicule* vehicule);
 
 double calculerTaxeAnnuelle(const Vehicule* vehicule) {
    assert(vehicule);
 
-   double montant = 0.;
-
    switch (vehicule->typeVehicule) {
       case VOITURE:
-         // Taxe de base
-         montant += TAXE_BASE_VOITURE_CHF;
-
-         // Taxe spécifique
-         switch (vehicule->voiture.typeVoiture) {
-            case STANDARD:
-               if (vehicule->voiture.standard.cylindree
-                     < LIMITE_CYLINDREE_TAXE) {
-                  montant += vehicule->voiture.standard.quantiteRejetCO2
-                     < LIMITE_CO2_TAXE ? TAXE_POLLUTION_1 : TAXE_POLLUTION_2;
-               } else {
-                  montant += TAXE_POLLUTION_3
-                     * (double)vehicule->voiture.standard.cylindree;
-               }
-               break;
-            case HAUT_DE_GAMME:
-               if (vehicule->voiture.hautDeGamme.puissanceDuMoteur
-                     <= LIMITE_PUISSANCE_MOTEUR_TAXE) {
-                  montant += TAXE_PUISSANCE_MOTEUR_CHF;
-               } else {
-                  montant += TAXE_HAUTE_PUISSANCE_MOTEUR_CHF
-                     + (double)vehicule->voiture.poids / 1000.
-                     * TAXE_PUISSANCE_POIDS_CHF;
-               }
-               break;
-         }
+         return calculerTaxeVoiture(vehicule);
          break;
       case CAMIONETTE:
-         // Taxe de base
-         montant += TAXE_BASE_CAMIONETTE_CHF;
+         return calculerTaxeCamionette(vehicule);
+      default:
+         // On retourne -l'infine pour indiquer que le type n'est pas
+         // pris en charge.
+         return -INFINITY;
+   }
+}
 
-         // Taxe spécifique
-         montant += TAXE_CAMIONETTE_PAR_VOLUME_CHF
-            * vehicule->camionette.volumeTransport;
+double calculerTaxeVoitureStandard(const Vehicule* vehicule) {
+   assert(vehicule);
+
+   double montant = 0.;
+
+   if (vehicule->voiture.standard.cylindree < LIMITE_CYLINDREE_TAXE) {
+      montant += vehicule->voiture.standard.quantiteRejetCO2
+         < LIMITE_CO2_TAXE ? TAXE_POLLUTION_1 : TAXE_POLLUTION_2;
+   } else {
+      montant += TAXE_POLLUTION_3
+         * (double)vehicule->voiture.standard.cylindree;
+   }
+
+   return montant;
+}
+
+double calculerTaxeVoitureHautDeGamme(const Vehicule* vehicule) {
+   assert(vehicule);
+
+   double montant = 0.;
+
+   if (vehicule->voiture.hautDeGamme.puissanceDuMoteur
+         <= LIMITE_PUISSANCE_MOTEUR_TAXE) {
+      montant += TAXE_PUISSANCE_MOTEUR_CHF;
+   } else {
+      montant += TAXE_HAUTE_PUISSANCE_MOTEUR_CHF
+         + (double)vehicule->voiture.poids / 1000.
+         * TAXE_PUISSANCE_POIDS_CHF;
+   }
+
+   return montant;
+}
+
+double calculerTaxeVoiture(const Vehicule* vehicule) {
+   assert(vehicule);
+
+   double montant = 0.;
+
+   // Taxe de base
+   montant += TAXE_BASE_VOITURE_CHF;
+
+   switch (vehicule->voiture.typeVoiture) {
+      case STANDARD:
+         montant += calculerTaxeVoitureStandard(vehicule);
+         break;
+      case HAUT_DE_GAMME:
+         montant += calculerTaxeVoitureHautDeGamme(vehicule);
          break;
    }
+
+   return montant;
+}
+
+double calculerTaxeCamionette(const Vehicule* vehicule) {
+   assert(vehicule);
+
+   double montant = 0.;
+
+   // Taxe de base
+   montant += TAXE_BASE_CAMIONETTE_CHF;
+
+   // Taxe spécifique
+   montant += TAXE_CAMIONETTE_PAR_VOLUME_CHF
+      * vehicule->camionette.volumeTransport;
 
    return montant;
 }
